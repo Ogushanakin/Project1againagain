9fbc93b8c24d717ae49a2c619e9875615dcfd7a7
ogushan-akin <ogzhnakn96@gmail.com>
2022-01-06 11:27:35 +0300

    Initial Commit

---
+++ Project1againagain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1againagain
+//
+//  Created by AKIN on 6.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
