4727cde3cfcd35902f421ab50937adbad9a7cf19
Oğuzhan Akın <55364051+Ogushanakin@users.noreply.github.com>
2022-01-29 13:42:01 +0300

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Project1againagain
+//
+//  Created by AKIN on 6.01.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default //dosya sistemiyle çalışmamıza izin veren bir veri türü
+        let path = Bundle.main.resourcePath! //Uygulamaya yüklediğim resimleri nerede bulabileceğimi söyle
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items { //her öğeyi birkez yürütecek döngü
+            if item.hasPrefix("nssl") {
+                //this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
